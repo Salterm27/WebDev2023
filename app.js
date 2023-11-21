31a73a22aa796b6306399a11bea3f178d6099a3a
Salterm27 <sebastian.alterman@gmail.com>
2023-11-21 09:00:09 -0300

    cart is displayed in modal

--- app.js
+++ app.js
@@ -12,43 +12,105 @@
         //stringifies an items payload
 
         // Read LocalStorage. If item is not there, add then Push to Cart. If item is there, dont add and alert user.
-        function Add2Cart(){
-            console.log('button pressed');
-            let i;
-            let item = 'test';
+        function Add2Cart(item){
+            let myCart = document.getElementById('cart');
+
             if (localStorage.getItem(item) != null ){
                 alert('Ya estas anotado! Verifica Tus Cursos.');
             }
             else {
-                console.log('Before Adding'+localStorage.length);
+                //add2LocalStorage
                 localStorage.setItem(item,'payload');
-                console.log('After Adding');
+
+                //add2CartDisplay
+                //create a new node
+                let myNewItem = SimpleElementBuilder('li','cart-item','');
+                    myNewItem.id = item;
+
+                    //create the description
+                    let itsChild = SimpleElementBuilder ('h4','cart-item-text',item);
+                    myNewItem.appendChild(itsChild);
+                    
+                    // create the button for the cart remove
+                    itsChild = SimpleElementBuilder('button','cart-item-remove','Desinscribirme');
+                    //attatch the buttons event listener
+                    itsChild.addEventListener('click', (e) => {RemoveFromCart(item)});
+                    
+                    myNewItem.appendChild(itsChild);
+                    
+
+                //insert the new node with its content into the cart
+
+                myCart.appendChild(myNewItem);
+                
                 UpdateCart(localStorage);
             }
         }
+
+        function SimpleElementBuilder(aType, aClass, anAttribute){
+            let newElement = document.createElement(aType);
+                if(aClass != ''){
+                    newElement.className = aClass;
+                }
+                if(anAttribute != ''){
+                    newElement.innerHTML = anAttribute;
+                }                
+            return newElement;
+        }
+
         
         function UpdateCart(tempFiles){
             // If Empty, do nothing. If Has Something,  enable Cart
             if (tempFiles.length === 0){
-                console.log('Local Storage is Empty');
                 document.getElementById('MisCursos').className= 'navbar-link-disabled';
                 document.getElementById('MisCursos').innerHTML = 'Mis Cursos (0)';
+
+
             }
             else{
-                console.log('Local Storage has something in it')
                 document.getElementById('MisCursos').className= 'navbar-link';
                 document.getElementById('MisCursos').innerHTML= 'Mis Cursos ('+ tempFiles.length +')';
-                
+
             }
         }
-    //TODO ShowCart
-        //displays all elements in the cart
-    function ShowCart(tempFiles){
+        //logs all elements in the cart
+    function LogCart(tempFiles){
         let i;
         for(i=0; i<tempFiles.length;i++){
             console.log(tempFiles.key(i));
 
         }
+    }
+    
+    // Removes a Line in a Table
+    function RemoveFromCart (item){
+            if (localStorage.getItem(item) === null ){
+                alert('No estas anotado! Anotate desde el Home.');
+            }
+            else {
+                //remove from LS
+                localStorage.removeItem(item);
+                
+                //remove from Cart Display
+                let myCartItem = document.getElementById(item);
+                myCartItem.remove();
+
+                //updateCart
+                UpdateCart(localStorage);
+            }
+            
+
+    }
+
+    function flushCart(){
+        
+        while (localStorage.key(0)!=null){
+            
+            //console.log(localStorage.key(0));
+            RemoveFromCart(localStorage.key(0));
+            
+        }
+        
     }
 
 
