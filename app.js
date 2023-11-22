b89971d22f90be6cfcb106c6e5201fe2968f6305
Salterm27 <sebastian.alterman@gmail.com>
2023-11-22 17:32:41 -0300

    Reorg'd App.JS Hierarchy. Added fix to onload with full cart. Separated CartDisplay Functions from add functions

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
     //TODO OnloadOrchestration
     let tempFiles = window.localStorage;   
     let myCart; 
-    UpdateCart(tempFiles);
+    mountCart();
     
     //TODO populateCourses.
         // Read from JSON Config file and add the Banner CTA List. Attatch the events
@@ -11,107 +11,115 @@
     //TODO PackageItem
         //stringifies an items payload
 
+    
+    function Add2Cart(item){
         // Read LocalStorage. If item is not there, add then Push to Cart. If item is there, dont add and alert user.
-        function Add2Cart(item){
-            let myCart = document.getElementById('cart');
+        let myCart = document.getElementById('cart');
 
-            if (localStorage.getItem(item) != null ){
-                alert('Ya estas anotado! Verifica Tus Cursos.');
-            }
-            else {
-                //add2LocalStorage
-                localStorage.setItem(item,'payload');
-
-                //add2CartDisplay
-                //create a new node
-                let myNewItem = SimpleElementBuilder('li','cart-item','');
-                    myNewItem.id = item;
-
-                    //create the description
-                    let itsChild = SimpleElementBuilder ('h4','cart-item-text',item);
-                    myNewItem.appendChild(itsChild);
-                    
-                    // create the button for the cart remove
-                    itsChild = SimpleElementBuilder('button','cart-item-remove','Desinscribirme');
-                    //attatch the buttons event listener
-                    itsChild.addEventListener('click', (e) => {RemoveFromCart(item)});
-                    
-                    myNewItem.appendChild(itsChild);
-                    
-
-                //insert the new node with its content into the cart
-
-                myCart.appendChild(myNewItem);
-                
-                UpdateCart(localStorage);
-            }
+        if (localStorage.getItem(item) != null ){
+            alert('Ya estas anotado! Verifica Tus Cursos.');
         }
-
-        function SimpleElementBuilder(aType, aClass, anAttribute){
-            let newElement = document.createElement(aType);
-                if(aClass != ''){
-                    newElement.className = aClass;
-                }
-                if(anAttribute != ''){
-                    newElement.innerHTML = anAttribute;
-                }                
-            return newElement;
-        }
-
-        
-        function UpdateCart(tempFiles){
-            // If Empty, do nothing. If Has Something,  enable Cart
-            if (tempFiles.length === 0){
-                document.getElementById('MisCursos').className= 'navbar-link-disabled';
-                document.getElementById('MisCursos').innerHTML = 'Mis Cursos (0)';
-
-
-            }
-            else{
-                document.getElementById('MisCursos').className= 'navbar-link';
-                document.getElementById('MisCursos').innerHTML= 'Mis Cursos ('+ tempFiles.length +')';
-
-            }
-        }
-        //logs all elements in the cart
-    function LogCart(tempFiles){
-        let i;
-        for(i=0; i<tempFiles.length;i++){
-            console.log(tempFiles.key(i));
-
+        else {
+            //add to LocalStorage
+            localStorage.setItem(item,'payload');               
+            
+            //add to cart display list 
+            addCartDisplayItem(myCart,item);            
         }
     }
-    
-    // Removes a Line in a Table
-    function RemoveFromCart (item){
-            if (localStorage.getItem(item) === null ){
-                alert('No estas anotado! Anotate desde el Home.');
-            }
-            else {
-                //remove from LS
-                localStorage.removeItem(item);
+
+    function addCartDisplayItem(myCart,item){
+        let myNewItem = SimpleElementBuilder('li','cart-item','');
+        myNewItem.id = item;
+        //create the description
+        let itsChild = SimpleElementBuilder ('h4','cart-item-text',item);
+        myNewItem.appendChild(itsChild);
+        
+        // create the button for the cart remove
+        itsChild = SimpleElementBuilder('button','cart-item-remove','Desinscribirme');
+        
+        //attatch the buttons event listener
+        itsChild.addEventListener('click', (e) => {RemoveFromCart(item)});
+        myNewItem.appendChild(itsChild);
+        
+        //insert the new node with its content into the cart
+        myCart.appendChild(myNewItem);
                 
-                //remove from Cart Display
-                let myCartItem = document.getElementById(item);
-                myCartItem.remove();
+        UpdateCart(localStorage);
+    }
 
-                //updateCart
-                UpdateCart(localStorage);
-            }
-            
 
+    function RemoveFromCart (item){
+        // Removes an item from the cart
+        if (localStorage.getItem(item) === null ){
+            alert('No estas anotado! Anotate desde el Home.');
+        }
+        else {
+        //remove from LS
+        localStorage.removeItem(item);
+                
+        //remove from Cart Display
+        removeCardDisplayItem(item);   
+        }
+    }
+
+    function removeCardDisplayItem(item){
+        //Remove from Cart Display List
+        let myCartItem = document.getElementById(item);
+        myCartItem.remove();
+                
+        //updateCart
+        UpdateCart(localStorage);
     }
 
     function flushCart(){
-        
+        //removes everything from the cart
+
         while (localStorage.key(0)!=null){
-            
-            //console.log(localStorage.key(0));
             RemoveFromCart(localStorage.key(0));
-            
         }
         
     }
+    function mountCart(tempFiles){
+        let i;
+        let myCart = document.getElementById('cart');
+        if (localStorage.length > 0){
+            for(i=0; i<localStorage.length; i++ ){
+                addCartDisplayItem(myCart,localStorage.key(i));
+            }
+        }
+        UpdateCart(tempFiles)
 
+    }
+    function UpdateCart(tempFiles){
+        // If Empty, do nothing. If Has Something,  enable Cart
+        if (tempFiles.length === 0){
+            document.getElementById('MisCursos').className= 'navbar-link-disabled';
+            document.getElementById('MisCursos').innerHTML = 'Mis Cursos (0)';
 
+        }
+        else{
+            document.getElementById('MisCursos').className= 'navbar-link';
+            document.getElementById('MisCursos').innerHTML= 'Mis Cursos ('+ tempFiles.length +')';
+        }
+    }
+
+    function LogCart(tempFiles){
+        //logs all elements in the cart
+        let i;
+        for(i=0; i<tempFiles.length;i++){
+            console.log(tempFiles.key(i));
+        }
+    }
+
+    function SimpleElementBuilder(aType, aClass, anAttribute){
+        let newElement = document.createElement(aType);
+            if(aClass != ''){
+                newElement.className = aClass;
+            }
+            if(anAttribute != ''){
+                newElement.innerHTML = anAttribute;
+            }                
+        return newElement;
+    }
     
